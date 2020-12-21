01b36ce8ffd3a829dbf0302ddb113f5d3c2def68
Akash Lakhani <180420116023.it18@scet.ac.in>
2020-12-21 10:11:04 +0530

    third commit

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelector('p').addEventListener('click', function()  {
-    var name = prompt('Enter a new name');
-    document.querySelector('p').textContent = 'Player 1: ' + name;
-    
-    });
-
